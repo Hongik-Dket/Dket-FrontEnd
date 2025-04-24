68696cfcf6c84bcc0e4b60889a962a116862a15f
이지우 <insa658723@gmail.com>
2025-04-24 17:07:28 +0900

    fix: 공연 상세리스트 UI 수정

--- Dket_iOS/Views/Host/HostedEventListView.swift
+++ Dket_iOS/Views/Host/HostedEventListView.swift
@@ -8,16 +8,31 @@
 import SwiftUI
 
 struct HostedEventListView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        ScrollView {
-            VStack {
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 12) {
                 ForEach(0..<10) { _ in
-                    EventCardView()
+                    VerticalEventCardView()
                         .padding(.bottom, 10)
                 }
             }
             .padding()
         }
         .navigationTitle("개최한 공연")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
+                        .font(.system(size: 17, weight: .semibold))
+                }
+            }
+        }
     }
 }
