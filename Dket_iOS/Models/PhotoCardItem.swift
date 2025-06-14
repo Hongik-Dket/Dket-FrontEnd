b982e4578b1789b5741d10dd13a2c3c08ac48cdb
이지우 <insa658723@gmail.com>
2025-06-15 05:42:05 +0900

    feat: 전체 포토카드 목록 조회 및 티켓번호로 조회하기 뷰 생성

---
+++ Dket_iOS/Models/PhotoCardItem.swift
@@ -0,0 +1,11 @@
+//
+//  PhotoCard.swift
+//  Dket_iOS
+//
+//  Created by 이지우 on 6/15/25.
+//
+
+struct PhotoCard: Identifiable {
+    let id: Int64
+    let imageUrl: String
+}
---
+++ Dket_iOS/Networking/PhotoCardItemDTO.swift
@@ -0,0 +1,11 @@
+//
+//  Untitled.swift
+//  Dket_iOS
+//
+//  Created by 이지우 on 6/15/25.
+//
+
+struct PhotoCardItemDTO: Decodable {
+    let photoCardId: Int64
+    let imageUrl: String
+}
